d52ac3d9e3050e5d1d319067c1e5b5730d7cec70
Jim Christodoulou <jimchrist2@gmail.com>
2021-10-10 12:56:57 +0300

    modified:   humidity/humidity.ino

--- humidity/humidity.ino
+++ humidity/humidity.ino
@@ -2,37 +2,39 @@
 int hmeter=A0;
 double hval=0.0;
 int blue_led=3, yel_led=5, red_led=6;
-int ledval;
+int _delay=1;
+int samples=10000;
+
+
 void setup() {
   Serial.begin(9600);
   delay(1000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  for (int i=0; i<300;i++) {
+  hval=0.0;
+  for (int i=0; i<samples;i++) {
     hval+=analogRead(hmeter);
-    delay(10);
+    delay(_delay);
   }
-  hval = hval/300;
+  hval = hval/samples;
   Serial.println(hval);
-  if (hval>30) {
+  if (hval>30 && hval<50) {
     digitalWrite(blue_led, HIGH);
   }
   else {
     digitalWrite(blue_led, LOW);
   }
-  if (hval>50) {
+  if (hval>50 && hval<70) {
     digitalWrite(yel_led, HIGH);
   }
   else {
-    digitalWrite(blue_led, LOW);
+    digitalWrite(yel_led, LOW);
   }
-  if (hval>60) {
+  if (hval>70) {
     digitalWrite(red_led, HIGH);
   }
   else {
-    digitalWrite(blue_led, LOW);
+    digitalWrite(red_led, LOW);
   }
-  hval=0;
 }
